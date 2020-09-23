177f4421969b761196379635ce5ec6b0a5f3c1dd
Dzy0123 <1029333244@qq.com>
2020-09-23 13:22:51 +0800

    基础知识day01

---
+++ src/djc/day01/HelloWorld.java
@@ -0,0 +1,11 @@
+package djc.day01;
+/*多行注释
+* 快捷键:ctrl+shift+?
+* */
+//单行注释
+//快捷键：ctrl+?
+public class HelloWorld { //定义类名
+    public static void main(String[] args){ //main方法，代表程序执行的起点
+        System.out.println("Hellow World！"); //打印输出语句
+    }
+}
